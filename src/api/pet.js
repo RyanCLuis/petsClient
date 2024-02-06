f67ddee6afa4392fdf582a9f11671bd7a5258249
Ryan Luis <RyanCLuis@gmail.com>
2024-02-06 08:51:45 -0800

    can now type in the modal and get it to display on the page

--- src/api/pet.js
+++ src/api/pet.js
@@ -27,6 +27,7 @@ export const createPet = (user, newPet) => {
 }
 
 // UPDATE -> Edit a pet
+export const updatePet = () => { console.log('pet update')}
 // DELETE -> Set a pet free
 export const removePet = (user, id) => {
     return axios({
